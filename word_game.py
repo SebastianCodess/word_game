4a0d8d7a4598df6e83186fea17452e09dea07ff0
Sebastian Garcia <sgaric10@terpmail.umd.edu>
2022-11-01 13:16:33 -0400

    added word_game.py

---
+++ word_game.py
@@ -0,0 +1,19 @@
+""" This is the creation of the Word Game we are building in INST326!"""
+
+class Word: 
+    """ A word in the word game!
+    
+    Attributes: 
+          constonants (str): The constonants in the word 
+          vowels(str): The vowels in the word
+    """
+    pass
+
+    """ To show the professor that you have collaborated in this assignment, 
+    Please add your name to the list I have created below. I will start with my
+    name, Sebastian Garcia. You can hard code your name in the list or append it
+    using the append method. Thank You!
+    
+    """
+    names = ["Sebastian Garcia"]
+    print(names) 
